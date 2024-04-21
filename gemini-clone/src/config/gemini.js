03b19f4a255e8f40b653ea29bacd4c4521f0932c
Aaron <a.genevision14@gmail.com>
2024-04-21 02:44:28 -0500

    Completed Google Gemini API functionality and implementation

--- gemini-clone/src/config/gemini.js
+++ gemini-clone/src/config/gemini.js
@@ -2,11 +2,11 @@
 // node --version # Should be >= 18
 // npm install @google/generative-ai
 
-const {
+import {
     GoogleGenerativeAI,
     HarmCategory,
     HarmBlockThreshold,
-  } = require("@google/generative-ai");
+  } from "@google/generative-ai";
   
   const MODEL_NAME = "gemini-1.0-pro";
   const API_KEY = "AIzaSyDEPOEd6Llfx5wkGfbTDlCdowOrvEzioiI";
@@ -51,6 +51,7 @@ const {
     const result = await chat.sendMessage(prompt);
     const response = result.response;
     console.log(response.text());
+    return response.text();
   }
   
   export default runChat;
