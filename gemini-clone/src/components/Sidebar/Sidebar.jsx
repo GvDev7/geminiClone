73b9970b8baf06f85fb0a076af00fd5e78d591ea
Aaron <a.genevision14@gmail.com>
2024-04-21 00:35:23 -0500

    Completed Sidebar component functionality, added Outfit google font

--- gemini-clone/src/components/Sidebar/Sidebar.jsx
+++ gemini-clone/src/components/Sidebar/Sidebar.jsx
@@ -1,36 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Sidebar.css";
 import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className='sidebar'>
         <div className="top">
-            <img className='menu' src={assets.menu_icon} alt="menu icon" />
+            <img className='menu' src={assets.menu_icon} alt="menu icon" onClick={()=>setIsOpen(!isOpen)}/>
             <div className="new-chat">
                 <img src={assets.plus_icon} alt="plus symbol to add new chat" />
-                <p>New Chat</p>
+                {isOpen?<p>New Chat</p>:null}
             </div>
-            <div className="recent">
-                <p className="recent-title">Recent</p>
-                <div className="recent-entry">
-                    <img src={assets.message_icon} alt="message box icon" />
-                    <p>Can AI help my company?...</p>
+            {isOpen?
+                <div className="recent">
+                    <p className="recent-title">Recent</p>
+                    <div className="recent-entry">
+                        <img src={assets.message_icon} alt="message box icon" />
+                        <p>Can AI help my company?...</p>
+                    </div>
                 </div>
-            </div>
+            : null}
         </div>
         <div className="bottom">
             <div className="bottom-item recent-entry">
                 <img src={assets.question_icon} alt="question mark icon" />
-                <p>Help</p>
+                {isOpen?<p>Help</p>:null}
             </div>
             <div className="bottom-item recent-entry">
                 <img src={assets.history_icon} alt="question mark icon" />
-                <p>Activity</p>
+                {isOpen?<p>Activity</p>:null}
             </div>
             <div className="bottom-item recent-entry">
                 <img src={assets.setting_icon} alt="question mark icon" />
-                <p>Settings</p>
+                {isOpen?<p>Settings</p>:null}
             </div>
         </div>
     </div>
